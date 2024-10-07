c9f05cdcad37dcd933eabc96a063007a07a38fcd
marieas <marie.askjem@gmail.com>
2024-10-07 08:51:31 +0200

    lol

---
+++ loginView.js
@@ -0,0 +1,15 @@
+function drawLoginView(){
+    return /*HTML*/`
+    <div>
+    <input placeholder="Brukernavn">
+    <input type="password" placeholder="Passord">
+       
+    Har ikke en bruker? 
+        <div class="registerLink">
+        Registrer deg her!
+        </div>
+    </div>   
+    
+    `
+
+}
